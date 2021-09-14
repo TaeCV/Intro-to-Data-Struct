88d9896342165e72f034f31fd4e3666bc73487ff
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-09-14 18:09:11 +0700

    vector mirror

---
+++ Midterm/Vector Mirror/student.h
@@ -0,0 +1,14 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+template <typename T>
+void CP::vector<T>::mirror() {
+    //write your code here
+    ensureCapacity(mSize * 2);
+    for (int i = mSize; i < mSize * 2; ++i) {
+        mData[i] = mData[mSize * 2 - i - 1];
+    }
+    mSize *= 2;
+}
+
+#endif
