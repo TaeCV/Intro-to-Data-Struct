4a621a72c8fce7e6b33908a0b2611d755257afb1
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-09-14 18:22:41 +0700

    erase many

--- Midterm/Erase Many from Vector/student.h
+++ Midterm/Erase Many from Vector/student.h
@@ -1,10 +1,22 @@
 #ifndef __STUDENT_H_
 #define __STUDENT_H_
 
-
 template <typename T>
 void CP::vector<T>::erase_many(const std::vector<int> &pos) {
-  //write your code here
+    //write your code here
+    T *arr = new T[mSize - pos.size()]();
+    int c = 0;
+    for (int i = 0; i < mSize; i++) {
+        if (pos[c] == i && c < pos.size()) {
+            c++;
+            continue;
+        }
+        arr[i - c] = mData[i];
+    }
+    delete[] mData;
+    mData = arr;
+    mSize -= pos.size();
+    mCap = mSize;
 }
 
 #endif
