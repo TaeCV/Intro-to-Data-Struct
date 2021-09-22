b0abf828efcd98208d272d8488e21ff365d454f9
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-09-22 12:42:33 +0700

    Queue Equality

---
+++ Midterm/Queue Equality/student.h
@@ -0,0 +1,15 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+template <typename T>
+bool CP::queue<T>::operator==(const CP::queue<T> &other) const {
+    if (size() != other.size())
+        return false;
+    for (int i = 0; i < size(); ++i) {
+        if (mData[(mFront + i) % mCap] != other.mData[(other.mFront + i) % other.mCap])
+            return false;
+    }
+    return true;
+}
+
+#endif
