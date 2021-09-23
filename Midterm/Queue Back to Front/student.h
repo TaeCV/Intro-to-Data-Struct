9921f314fa420b524c88c099ee80991fe2afacc4
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-09-23 14:05:02 +0700

    Queue Back to Front

---
+++ Midterm/Queue Back to Front/student.h
@@ -0,0 +1,14 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <iostream>
+
+template <typename T>
+void CP::queue<T>::back_to_front() {
+    //write your code here
+    mFront = (mFront - 1 + mCap) % mCap;
+    mData[mFront] = mData[(mFront + mSize) % mCap];
+}
+
+#endif
--- temp.cpp
+++ temp.cpp
@@ -2,6 +2,95 @@
 
 using namespace std;
 
+class Library {
+protected:
+    vector<int> stock;                      //map from id to stock
+    map<pair<string, string>, int> barcode; // map from author/title to barcode
+public:
+    void registerNewBook(string author, string title) {
+    auto it = barcode.find( {
+            author, title}
+    }
+    if (it == barcode.end()) {
+        barcode[{author, title}] = stock.size();
+        stock.push_back(1);
+    } else {
+        stock[it->second]++;
+    }
+} void borrow(int barcode) {
+    stock[barcode]--;
+}
+void return (int barcode) {
+    stock[barcode]++;
+}
+int current_stock(string title, string author) {
+ auto it = stock.find( {
+        author, title}
+}
+if (it == stock.end())
+    return -1;
+return stock[it->second];
+}
+vector<pair<string, int>> books_by_author(string author) {
+    vector<pair<string, int>> result;
+    auto it = stock.find({author, ""});
+    while (it != stock.end() && it->first.first == author) {
+ result.push_back( {it->first.sefcond, stock[it->second] );
+            it++;
+ }
+ return result;
+    }
+}
+}
+
+template <typename T>
+vector<T> alternate(const vector<T> &v1, const vector<T> &v2) {
+    vector<T> output(v1.size() + v2.size());
+    int n = max(v1.size(), v2.size()), c = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i < v1.size())
+            output[c++] = v1[i];
+        if (i < v2.size())
+            output[c++] = v2[i];
+    }
+    return output;
+}
+
+void printGrandfatherGrandson(const vector<string> &name, const vector<pair<string, string>> &parent) {
+    map<string, vector<string>> m;
+    for (auto &x : parent)
+        m[x.first].push_back(x.second);
+    for (auto &each : name) {
+        bool haveGrandChild = false;
+        if (m.find(each) != m.end()) {
+            for (auto &child : m[each]) {
+                if (m.find(child) != m.end()) {
+                    if (!haveGrandChild) {
+                        cout << each << " is a grand father with the following grandson(s): ";
+                        haveGrandChild = true;
+                    }
+                    for (auto &grand : m[child])
+                        cout << grand << " ";
+                }
+            }
+        }
+        if (haveGrandChild)
+            cout << "\n";
+    }
+}
+
 int main() {
-    cout << -2 % 3;
+    vector<int> v1 = {1, 2, 3, 4};
+    vector<int> v2 = {8, 9, 10, 11, 12, 13};
+    vector<string> name = {"John", "Jack", "Leo", "Eric", "Robert"};
+    vector<pair<string, string>> parent = {{"Leo", "John"}, {"John", "Eric"}, {"John", "Robert"}, {"Robert", "Jack"}};
+    map<string, int> m;
+    m["abc"] = 2;
+    for (auto &x : m)
+        cout << x.first;
+    // printGrandfatherGrandson(name, parent);
+    // vector<int> v3 = alternate(v1, v2);
+    // for (auto &x : v3) {
+    //     cout << x << " ";
+    // }
 }
