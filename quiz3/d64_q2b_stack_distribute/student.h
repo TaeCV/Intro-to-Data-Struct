d09ca1c08e45425f8fbbcb7cd9ad38aec0c0835a
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-11-01 15:11:29 +0700

    quiz 3

--- quiz3/d64_q2b_stack_distribute/student.h
+++ quiz3/d64_q2b_stack_distribute/student.h
@@ -3,6 +3,19 @@
 
 template <typename T>
 std::vector<std::vector<T>> CP::stack<T>::distribute(size_t k) const {
-  //write your code here
+    // write your code here
+    std::vector<std::vector<T>> res(k);
+    int t = mSize / k, c = 0, idx = 0, add = 0;
+    if (mSize % k != 0)
+        add = mSize % k;
+    for (int i = mSize - 1; i >= 0; --i) {
+        res[idx].push_back(mData[i]);
+        ++c;
+        if (c == t + 1 || idx >= add && c == t) {
+            ++idx;
+            c = 0;
+        }
+    }
+    return res;
 }
 #endif
--- quiz3/d64_q2b_vector_rotate/student.h
+++ quiz3/d64_q2b_vector_rotate/student.h
@@ -1,9 +1,19 @@
 #ifndef __STUDENT_H_
 #define __STUDENT_H_
 
+#include <vector>
+
 template <typename T>
 void CP::vector<T>::rotate(iterator first, iterator last, size_t k) {
-  //write your code here
+    // write your code here
+    std::vector<T> tmp(first, first + k);
+    for (iterator it = first; it != last - k; ++it) {
+        *it = *(it + k);
+    }
+    iterator it = last - k;
+    for (int i = 0; i < k; ++i) {
+        *(it + i) = tmp[i];
+    }
 }
 
 #endif
