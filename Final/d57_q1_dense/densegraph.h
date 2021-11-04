723020d1daa72a9776e6dc955ff110a40deee397
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-11-04 10:03:21 +0700

    tree

--- Final/d57_q1_dense/densegraph.h
+++ Final/d57_q1_dense/densegraph.h
@@ -2,40 +2,67 @@
 #define __DENSE_GRAPH_H__
 
 // Your code here
+#include <set>
 #include <vector>
 using namespace std;
 class DenseGraph {
 public:
     DenseGraph() {
         // Your code here
+        n = 3;
+        isT = false;
     }
 
     DenseGraph(int n_in) {
         // Your code here
+        n = n_in;
+        isT = false;
     }
 
     DenseGraph(const DenseGraph &G) {
         // Your code here
+        n = G.n;
+        isT = G.isT;
+        e = G.e;
     }
 
     void AddEdge(int a, int b) {
         // Your code here
+        if (isT) {
+            e.insert({b, a});
+        } else {
+            e.insert({a, b});
+        }
     }
 
     void RemoveEdge(int a, int b) {
         // Your code here
+        auto it = isT ? e.find({b, a}) : e.find({a, b});
+        if (it != e.end()) {
+            if (isT) {
+                e.erase(it);
+            } else {
+                e.erase(it);
+            }
+        }
     }
 
     bool DoesEdgeExist(int a, int b) const {
         // Your code here
+        return isT ? e.find({b, a}) != e.end() : e.find({a, b}) != e.end();
     }
 
     DenseGraph Transpose() const {
         // Your code here
+        DenseGraph G(*this);
+        G.isT = !isT;
+        return G;
     }
 
 protected:
     int n;
     // Your code here
+    bool isT;
+    set<pair<int, int>> e;
 };
 #endif // __DENSE_GRAPH_H__
--- Final/d60_q3_same_tree/tree.h
+++ Final/d60_q3_same_tree/tree.h
@@ -1,20 +1,24 @@
 #ifndef TREE_H_INCLUDED
 #define TREE_H_INCLUDED
 
-class Tree{
-    class Node{
-        public:
-            friend class Tree;
-            Node() {data = -1; left = NULL; right = NULL;}
-            Node(const int x, Node* left, Node* right) : data(x), left(left), right(right) {}
+class Tree {
+    class Node {
+    public:
+        friend class Tree;
+        Node() {
+            data = -1;
+            left = NULL;
+            right = NULL;
+        }
+        Node(const int x, Node *left, Node *right) : data(x), left(left), right(right) {}
 
-        protected:
-            int data;
-            Node* left;
-            Node* right;
+    protected:
+        int data;
+        Node *left;
+        Node *right;
     };
-public:
 
+public:
     Tree() {
         mRoot = NULL;
         mSize = 0;
@@ -24,8 +28,9 @@ public:
         clear(mRoot);
     }
 
-    void clear(Node*& r) {
-        if (!r) return;
+    void clear(Node *&r) {
+        if (!r)
+            return;
         clear(r->left);
         clear(r->right);
         delete r;
@@ -34,25 +39,36 @@ public:
     void insert(int x) {
         insertAt(mRoot, x);
     }
-    bool isSameBinaryTree(Tree& t) {
+    bool isSameBinaryTree(Tree &t) {
         // Insert your code here
+        return mSize == t.mSize && checkSameNode(mRoot, t.mRoot);
     }
     // You can also put your code here
+    bool checkSameNode(Node *&n, Node *&other) {
+        if (n == NULL && other == NULL)
+            return true;
+        else if (n != NULL && other != NULL && n->data == other->data) {
+            return checkSameNode(n->left, other->left) && checkSameNode(n->right, other->right);
+        }
+        return false;
+    }
 
 protected:
-    void insertAt(Node*& r, int x) {
+    void insertAt(Node *&r, int x) {
         if (!r) {
-           r = new Node(x, NULL, NULL);
-           mSize++;
-           return;
+            r = new Node(x, NULL, NULL);
+            mSize++;
+            return;
         }
-        if (r->data == x) return; // Do nothing
-        if (x < r->data) insertAt(r->left, x); else
-        insertAt(r->right, x);
+        if (r->data == x)
+            return; // Do nothing
+        if (x < r->data)
+            insertAt(r->left, x);
+        else
+            insertAt(r->right, x);
     }
-    Node* mRoot;
+    Node *mRoot;
     int mSize;
-
 };
 
 #endif // TREE_H_INCLUDED
