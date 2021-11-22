87eccc32fc7ecc06c300e4d6f9bbd744e29bc4d5
Chayakorn Vongbunsin <taecv.work@gmail.com>
2021-11-22 17:22:07 +0700

    Update student.h

--- quiz4/d64_q2c_vector_less/student.h
+++ quiz4/d64_q2c_vector_less/student.h
@@ -5,18 +5,16 @@ template <typename T>
 bool CP::vector<T>::operator<(const CP::vector<T> &other) const {
     // write your code here
     //  if you use std::vector, your score will be half (grader will report score BEFORE halving)
-    if (mSize != other.mSize)
-        return mSize < other.mSize;
+    if (mSize == 0 && other.mSize)
+        return true;
     int i = 0;
-    bool isEqual = false;
-    while (i < mSize) {
-        if (mData[i] > other.mData[i])
-            return false;
-        else if (mData[i] == other.mData[i])
-            isEqual = true;
+    while (i < mSize && i < other.mSize && mData[i] == other.mData[i])
         ++i;
-    }
-    return !isEqual;
+    if (i < mSize && i >= other.mSize)
+        return false;
+    if (i >= mSize && i < other.mSize)
+        return true;
+    return mData[i] < other.mData[i];
 }
 
 #endif
